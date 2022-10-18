3f616247a0d483e73bdde8765e562f6bc0d00e1e
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2022-10-18 18:12:46 +0500

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.7.20"
+	id("maven-publish")
+}
+
+group = "com.github.chereshnyabtw"
+version = "1.0-rc1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
---
+++ src/main/kotlin/ru/chrshnv/bitrix24api/Client.kt
@@ -0,0 +1,7 @@
+package ru.chrshnv.bitrix24api
+
+class Client(val clientId: String, val clientSecret: String) {
+	fun setup(body: String) {
+
+	}
+}

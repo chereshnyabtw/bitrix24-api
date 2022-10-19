85d0ff561ee8bee95d42f5e9fbc93333cb215d1f
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2022-10-19 17:17:37 +0500

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ publishing {
 		create<MavenPublication>("maven") {
 			groupId = "com.github.chereshnyabtw"
 			artifactId = "bitrix24-api"
-			version = "1.1-rc1"
+			version = "1.1-rc2"
 
 			from(components["kotlin"])
 		}

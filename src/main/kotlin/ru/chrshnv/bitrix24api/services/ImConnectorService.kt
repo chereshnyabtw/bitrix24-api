a5996c470f2f51e6c52d6a90a917cf6a7b14b0c7
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2022-10-19 17:43:45 +0500

    imconnector activation

--- src/main/kotlin/ru/chrshnv/bitrix24api/services/ImConnectorService.kt
+++ src/main/kotlin/ru/chrshnv/bitrix24api/services/ImConnectorService.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.client.HttpStatusCodeException
 import ru.chrshnv.bitrix24api.config.RestTemplateConfig
 import ru.chrshnv.bitrix24api.config.Settings
+import ru.chrshnv.bitrix24api.models.ActivationHandler
 import ru.chrshnv.bitrix24api.models.ImConnectorRegister
 
 class ImConnectorService {
@@ -33,4 +34,30 @@ class ImConnectorService {
 
 		return null
 	}
+
+	fun activate(activation: ActivationHandler): ResponseEntity<String>? {
+		val restTemplate = RestTemplateConfig.getRestTemplate()
+
+		Settings.getInstance().connector = activation.connector
+		Settings.getInstance().lineId = activation.line
+
+		for (i in 1..3) {
+			try {
+				return restTemplate
+					.getForEntity(
+						"${Settings.getInstance().url}imconnector.activate/?Connector=${activation.connector}&Line=${activation.line}&Active=${activation.active.toInt()}&auth=${Settings.getInstance().accessToken}",
+						String::class.java
+					)
+			} catch (e: HttpStatusCodeException) {
+				if(i < 3 && e.statusCode == HttpStatus.UNAUTHORIZED)
+					continue
+				else
+					throw e
+			}
+		}
+
+		return null
+	}
+
+	private fun Boolean.toInt() = if (this) 1 else 0
 }
